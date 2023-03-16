635c8eaf5aed715449e195dc5be30f0939475df8
rakosnicekcz <matej@hulek.cz>
2023-03-16 21:48:57 +0100

    Added data model classes

---
+++ api/src/main/java/cz/vut/fit/pis/xmatej55/data/Meeting.java
@@ -0,0 +1,118 @@
+package cz.vut.fit.pis.xmatej55.data;
+
+import java.sql.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.JoinColumn;
+
+@Entity
+@Table(name = "Meeting")
+public class Meeting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "subject")
+    private String subject;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "start")
+    private Date start;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "end")
+    private Date end;
+
+    @Column(name = "notes")
+    private String notes;
+    
+    @ManyToMany(fetch=FetchType.EAGER)
+    @JoinTable(
+        name = "MeetingEmployee",
+        joinColumns = {@JoinColumn(name="meeting_id")},
+        inverseJoinColumns = {@JoinColumn(name="employee_id")}
+    )
+    private Set<Employee> employees = new HashSet<Employee>();
+    
+    @ManyToOne(fetch=FetchType.EAGER)
+    private Client client;
+    
+    public Meeting() {}
+
+    public Meeting(String subject, Date start, Date end, String notes) {
+        this.subject = subject;
+        this.start = start;
+        this.end = end;
+        this.notes = notes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Set<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Set<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+}
---
+++ api/src/main/java/cz/vut/fit/pis/xmatej55/data/Product.java
@@ -0,0 +1,59 @@
+package cz.vut.fit.pis.xmatej55.data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Product")
+public class Product {
+
+    public Product(Long id, String name, Set<ClientProduct> clientProducts) {
+        this.id = id;
+        this.name = name;
+        this.clientProducts = clientProducts;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "name")
+    private String name;
+    
+    @OneToMany(mappedBy="product", fetch=FetchType.EAGER, cascade=CascadeType.ALL)
+    private Set<ClientProduct> clientProducts = new HashSet<ClientProduct>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<ClientProduct> getClientProducts() {
+        return clientProducts;
+    }
+
+    public void setClientProducts(Set<ClientProduct> clientProducts) {
+        this.clientProducts = clientProducts;
+    }
+}
