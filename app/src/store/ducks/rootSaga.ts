774c893d85455909da918f5808f72b9944d7c62d
Simon Košina <simonkosina@gmail.com>
2023-04-24 19:54:01 +0200

    meeting store

--- app/src/store/ducks/rootSaga.ts
+++ app/src/store/ducks/rootSaga.ts
@@ -2,10 +2,12 @@ import { all } from 'redux-saga/effects';
 
 import employeeSagas from './employee/sagas';
 import clientSagas from './client/sagas';
+import meetingSagas from './meeting/sagas';
 
 export default function* rootSaga(): Generator {
   return yield all([
     ...employeeSagas,
     ...clientSagas,
+    ...meetingSagas,
   ]);
 };
