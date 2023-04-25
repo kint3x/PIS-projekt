59b3cb01588ead9d9894db6362e7ed350e9bf2e8
Simon Košina <simonkosina@gmail.com>
2023-04-25 15:39:41 +0200

    register product store

--- app/src/store/ducks/rootReducer.ts
+++ app/src/store/ducks/rootReducer.ts
@@ -2,9 +2,12 @@ import { combineReducers } from 'redux';
 import employee from './employee';
 import client from './client';
 import meeting from './meeting';
+import product from './product';
+
 
 export default combineReducers({
   employee,
   client,
-  meeting
+  meeting,
+  product
 });
--- app/src/store/ducks/rootSaga.ts
+++ app/src/store/ducks/rootSaga.ts
@@ -3,11 +3,13 @@ import { all } from 'redux-saga/effects';
 import employeeSagas from './employee/sagas';
 import clientSagas from './client/sagas';
 import meetingSagas from './meeting/sagas';
+import productSagas from './product/sagas';
 
 export default function* rootSaga(): Generator {
   return yield all([
     ...employeeSagas,
     ...clientSagas,
     ...meetingSagas,
+    ...productSagas
   ]);
 };
