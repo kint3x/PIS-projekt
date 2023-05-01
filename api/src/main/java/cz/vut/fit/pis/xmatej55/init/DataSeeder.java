cce7b811535c572c5e5cfba5f869d3069b15cafd
Adam Rybansky <adam.rybansky@centrum.sk>
2023-05-01 09:19:17 +0200

    documentation and other test users created

--- api/src/main/java/cz/vut/fit/pis/xmatej55/init/DataSeeder.java
+++ api/src/main/java/cz/vut/fit/pis/xmatej55/init/DataSeeder.java
@@ -26,11 +26,45 @@ public class DataSeeder {
             admin.setDob(new Date());
             admin.setPhone("+421918420420");
             admin.setName("John");
-            admin.setSurname("Doe");
+            admin.setSurname("Adminovitch");
             admin.setImage("https://bootdey.com/img/Content/avatar/avatar6.png");
             admin.setType(EmployeeType.Owner);
 
             employeeService.create(admin);
+        }
+
+        Optional<Employee> existingWorker = employeeService.findByUsername("worker");
+        if (!existingWorker.isPresent()) {
+            Employee worker = new Employee();
+            worker.setUsername("worker");
+            worker.setPassword("worker");
+            worker.setEmail("worker@worker.com");
+            worker.setAddress("workerAddress");
+            worker.setDob(new Date());
+            worker.setPhone("+421918420421");
+            worker.setName("John");
+            worker.setSurname("Workerino");
+            worker.setImage("https://bootdey.com/img/Content/avatar/avatar1.png");
+            worker.setType(EmployeeType.Owner);
+
+            employeeService.create(worker);
+        }
+
+        Optional<Employee> existingManager = employeeService.findByUsername("manager");
+        if (!existingManager.isPresent()) {
+            Employee manager = new Employee();
+            manager.setUsername("manager");
+            manager.setPassword("manager");
+            manager.setEmail("manager@manager.com");
+            manager.setAddress("managerAddress");
+            manager.setDob(new Date());
+            manager.setPhone("+421918420422");
+            manager.setName("John");
+            manager.setSurname("Managerski");
+            manager.setImage("https://bootdey.com/img/Content/avatar/avatar4.png");
+            manager.setType(EmployeeType.Owner);
+
+            employeeService.create(manager);
         }
     }
 }
